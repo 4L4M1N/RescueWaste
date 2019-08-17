c9fabc7626d332c0d66cb5a9649fb298e449f32e
4L4M1N <mohammadamin601@gmail.com>
2019-08-17 18:47:29 +0600

    role changed

--- RescueWaste.API/Models/Role.cs
+++ RescueWaste.API/Models/Role.cs
@@ -3,8 +3,8 @@ namespace RescueWaste.API.Models
     public class Role
     {
         public const string AdminUsr = "Admin";
-        public const string AreaManagerUsr = "AreaManager";
-        public const string GeneralUsr = "GeneralUser";
+        public const string AreaManagerUsr = "Manager";
+        public const string GeneralUsr = "Rescuer";
 
     }
 }
