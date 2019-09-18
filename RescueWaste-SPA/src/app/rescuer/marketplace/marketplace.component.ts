7a37a6d2681c3a87c774dfe313d8b1a7785c4d3c
4L4M1N <mohammadamin601@gmail.com>
2019-09-19 00:42:16 +0600

    Show promocodes

--- RescueWaste-SPA/src/app/rescuer/marketplace/marketplace.component.ts
+++ RescueWaste-SPA/src/app/rescuer/marketplace/marketplace.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Promocode } from 'src/app/_models/Promocode';
+import { PromocodeService } from 'src/app/services/promocode.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-marketplace',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./marketplace.component.css']
 })
 export class MarketplaceComponent implements OnInit {
-
-  constructor() { }
+  promocodes: Promocode[];
+  constructor(private promocodeService: PromocodeService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.loadPromocode();
+  }
+  loadPromocode()
+  {
+    this.promocodeService.getPromocodes().subscribe((promocodes: Promocode[]) => {
+      this.promocodes = promocodes;
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
