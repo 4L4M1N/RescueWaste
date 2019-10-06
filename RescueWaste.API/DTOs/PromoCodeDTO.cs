736ffd77d8752fc30039946f20a1376bd9c35926
4L4M1N <mohammadamin601@gmail.com>
2019-10-06 20:02:42 +0600

    required attribute added

--- RescueWaste.API/DTOs/PromoCodeDTO.cs
+++ RescueWaste.API/DTOs/PromoCodeDTO.cs
@@ -10,17 +10,24 @@ namespace RescueWaste.API.DTOs
     public class PromoCodeDTO
     {
        //TODO: Required attribute need to add 
+        [Required]
         public string  Name { get; set; }
         
+        [Required]
         public DateTime ExpireDate { get; set; }
         public bool IsActive { get; set; }
     
         public byte MerchantID { get; set; }
         public string Url { get; set; }
+        [Required]
         public IFormFile File { get; set; }
         public string PublicId { get; set; }
         public string AreaManagerID { get; set; }
+        
+        [Required]
         public double Discount { get; set; }
+        
+        [Required]
         public int CoinsRequired { get; set; }
         
     }
