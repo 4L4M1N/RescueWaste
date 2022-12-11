0fd0594e04efb10c67e7037ac4c22e569193cb4a
4L4M1N <mohammad.al.amin@outlook.com>
2022-12-11 23:02:14 +0600

    add extention method for configure services

---
+++ src/Identity/Identity.API/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+namespace Identity.API.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+    }
+}
